5dc4677748f61d79dfab8ed5f1192f4146914dae
Hunter McDonald <75329013+SpookySquid14@users.noreply.github.com>
2025-09-06 15:19:44 -0400

    Update motion_state.gd

--- Scripts/motion_state.gd
+++ Scripts/motion_state.gd
@@ -3,6 +3,9 @@ class_name Motion_State
 
 const SPEED = 5.0
 const SENSITIVITY = .003
+const BOB_FREQUENCY = 2.0
+const BOB_AMP =0.08
+var bobbing
 
 func motion_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
